7a8659ab99b2d3d10f85f8c208ffe74ad54cfda6
The Firefox Foundation <reinmclaren33@gmail.com>
2020-05-29 10:23:15 +0700

    Update run test TechOS script file
    Bochs
    QEMU
    Virtual PC

--- test-win_vpc.bat
+++ test-win_vpc.bat
@@ -1,12 +1,12 @@
 @echo off
 echo Starting using Virtual PC . . . .
 if not "%PROCESSOR_ARCHITECTURE%"=="X86" goto 64bit
-start /wait "%programfiles%\Microsoft Virtual PC\virtual pc.exe" -startvm "%cd%\emulator\TechOS.vmc"
+call "%programfiles%\Microsoft Virtual PC\virtual pc.exe" -startvm "%cd%\emulator\TechOS.vmc"
 if errorlevel 1 goto error
 exit
 
 :64bit
-start /wait "%programfiles(x86)%\Microsoft Virtual PC\virtual pc.exe" -startvm "%cd%\emulator\TechOS.vmc"
+call "%programfiles(x86)%\Microsoft Virtual PC\virtual pc.exe" -startvm "%cd%\emulator\TechOS.vmc"
 if errorlevel 1 goto error
 exit
 
